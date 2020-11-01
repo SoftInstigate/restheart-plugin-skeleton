5f5b0ed2489716259e2ff15988764795c795ee53
Andrea Di Cesare <andrea@softinstigate.com>
2020-11-01 11:14:16 +0100

    Don't restart container if build fails

--- bin/watch.sh
+++ bin/watch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find ./src -name "*.java" | entr sh -c 'mvn package; docker stop restheart; docker start restheart'
+find ./src -name "*.java" | entr sh -c 'mvn clean package && docker stop restheart && docker start restheart'
