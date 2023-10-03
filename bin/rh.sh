a0e866927360c36305da555d37b0684e858096f4
Andrea Di Cesare <andrea@softinstigate.com>
2023-10-03 10:47:28 +0200

    Update rh.sh

--- bin/rh.sh
+++ bin/rh.sh
@@ -261,9 +261,9 @@ _kill() {
 _run() {
     if [ -z ${RHO:-""} ]; then
         if _only_print_conf; then
-            RHO="/http-listner/port->${http_port};" java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:$((http_port+1000)) -jar "${rh_dir}"/restheart.jar ${options}
+            RHO="/http-listener/port->${http_port};" java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:$((http_port+1000)) -jar "${rh_dir}"/restheart.jar ${options}
         else
-            RHO="/http-listner/port->${http_port};" nohup java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:$((http_port+1000)) -jar "${rh_dir}"/restheart.jar ${options} > "${repo_dir}"/restheart.log &
+            RHO="/http-listener/port->${http_port};" nohup java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:$((http_port+1000)) -jar "${rh_dir}"/restheart.jar ${options} > "${repo_dir}"/restheart.log &
             msg "${YELLOW}RESTHeart starting${NOFORMAT}"
             started=0
             for i in {1..5}; do
